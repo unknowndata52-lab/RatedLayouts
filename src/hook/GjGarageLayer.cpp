51f1392086471bf570aa168db692059499e20a9d
iAndyHD3 <54410739+iAndyHD3@users.noreply.github.com>
2026-01-08 10:56:01 +0100

    fix crash

--- src/hook/GjGarageLayer.cpp
+++ src/hook/GjGarageLayer.cpp
@@ -14,7 +14,14 @@ class $modify(GJGarageLayer) {
             CCNode* planetsValue = nullptr;
             int storedStars = 0;
             int storedPlanets = 0;
+            utils::web::WebTask profileTask;
+
       };
+
+      void onBack(CCObject* sender) {
+            m_fields->profileTask.cancel();
+            GJGarageLayer::onBack(sender);
+      }
 
       bool init() {
             if (!GJGarageLayer::init())
@@ -60,12 +67,10 @@ class $modify(GJGarageLayer) {
             // web request
             auto postReq = web::WebRequest();
             postReq.bodyJSON(jsonBody);
-            auto postTask = postReq.post("https://gdrate.arcticwoof.xyz/profile");
+            m_fields->profileTask = postReq.post("https://gdrate.arcticwoof.xyz/profile");
 
             // Handle the response
-            auto self = this;
-            postTask.listen([self](web::WebResponse* response) {
-                  if (!self) return;
+            m_fields->profileTask.listen([this](web::WebResponse* response) {
                   log::info("Received response from server");
 
                   if (!response->ok()) {
@@ -86,37 +91,37 @@ class $modify(GJGarageLayer) {
                   int stars = json["stars"].asInt().unwrapOrDefault();
 
                   log::info("Profile data - points: {}, stars: {}", points, stars);
-                  self->m_fields->storedStars = stars;
+                  m_fields->storedStars = stars;
 
                   // create/update the stat item now that we have the value
                   log::debug("Updating stat item with {} stars", stars);
-                  if (self->m_fields->statMenu) {
+                  if (m_fields->statMenu) {
                         // if an existing stat item exists, remove it first
-                        if (self->m_fields->starsValue) {
-                              self->m_fields->starsValue->removeFromParent();
-                              self->m_fields->starsValue = nullptr;
+                        if (m_fields->starsValue) {
+                              m_fields->starsValue->removeFromParent();
+                              m_fields->starsValue = nullptr;
                         }
-                        if (self->m_fields->planetsValue) {
-                              self->m_fields->planetsValue->removeFromParent();
-                              self->m_fields->planetsValue = nullptr;
+                        if (m_fields->planetsValue) {
+                              m_fields->planetsValue->removeFromParent();
+                              m_fields->planetsValue = nullptr;
                         }
 
                         auto starSprite = CCSprite::create("RL_starMed.png"_spr);
                         auto starsValue = StatsDisplayAPI::getNewItem(
                             "blueprint-stars"_spr, starSprite,
-                            self->m_fields->storedStars, 0.54f);
+                            m_fields->storedStars, 0.54f);
 
-                        self->m_fields->starsValue = starsValue;
-                        self->m_fields->statMenu->addChild(starsValue);
+                        m_fields->starsValue = starsValue;
+                        m_fields->statMenu->addChild(starsValue);
 
                         auto planetSprite = CCSprite::create("RL_planetMed.png"_spr);
                         auto planetsValue = StatsDisplayAPI::getNewItem(
                             "planets-collected"_spr, planetSprite,
                             planets, 0.54f);
-                        self->m_fields->planetsValue = planetsValue;
-                        self->m_fields->statMenu->addChild(planetsValue);
+                        m_fields->planetsValue = planetsValue;
+                        m_fields->statMenu->addChild(planetsValue);
                         // call updateLayout if available
-                        if (auto menu = typeinfo_cast<CCMenu*>(self->m_fields->statMenu)) {
+                        if (auto menu = typeinfo_cast<CCMenu*>(m_fields->statMenu)) {
                               menu->updateLayout();
                         }
                   } else {
