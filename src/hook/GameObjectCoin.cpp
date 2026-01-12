ec943055b7fe76ea2f9ca8a99e73b825cc038639
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2026-01-12 10:25:57 +0800

    blue coin temp

--- src/hook/GameObjectCoin.cpp
+++ src/hook/GameObjectCoin.cpp
@@ -1,16 +1,17 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCSprite.hpp>
 #include <Geode/modify/EffectGameObject.hpp>
-#include <Geode/modify/GameObject.hpp>
 
 using namespace geode::prelude;
 
 const int USER_COIN = 1329;
+const ccColor3B BRONZE_COLOR = ccColor3B{255, 175, 75};
 
 // Replace coin visuals when GameObjects are set up
 class $modify(EffectGameObject) {
       struct Fields {
             utils::web::WebTask m_fetchTask;
+            bool m_isSuggested = false;
             ~Fields() { m_fetchTask.cancel(); }
       };
 
@@ -29,11 +30,11 @@ class $modify(EffectGameObject) {
 
             int levelId = playLayer->m_level->m_levelID;
 
-            // fetch level info from server; if isSuggested == true OR request fails, DO NOT apply blue-coin visuals
             auto url = fmt::format("https://gdrate.arcticwoof.xyz/fetch?levelId={}", levelId);
-            Ref<GameObject> selfRef = this;
+            Ref<EffectGameObject> selfRef = this;
             m_fields->m_fetchTask = web::WebRequest().get(url);
-            m_fields->m_fetchTask.listen([selfRef, levelId](web::WebResponse* res) {
+            auto fields = m_fields;
+            m_fields->m_fetchTask.listen([selfRef, fields, levelId](web::WebResponse* res) {
                   if (!selfRef) return;
 
                   if (!res || !res->ok()) {
@@ -49,14 +50,46 @@ class $modify(EffectGameObject) {
 
                   auto json = jsonRes.unwrap();
                   bool isSuggested = json["isSuggested"].asBool().unwrapOrDefault();
+
                   if (isSuggested) {
-                        log::debug("skipping replacement", levelId);
+                        if (!selfRef->getChildByID("rl-suggested")) {
+                              auto marker = CCNode::create();
+                              marker->setID("rl-suggested");
+                              selfRef->addChild(marker);
+                        }
+
+                        log::debug("skipping replacement for level {}", levelId);
                         return;
+                  }
+
+                  if (auto marker = selfRef->getChildByID("rl-suggested")) {
+                        marker->removeFromParent();
                   }
 
                   // TODO: change the coin gameobject sprite to blue coin with animation pls
 
                   selfRef->m_addToNodeContainer = true;
             });
+      }
+};
+
+// detect and remove the bronze tint on unverified coins
+class $modify(CCSprite) {
+      void setColor(ccColor3B const& col) {
+            if (col == BRONZE_COLOR) {
+                  GameObject* gameObj = typeinfo_cast<GameObject*>(this);  // check for user coin
+                  if (gameObj && gameObj->m_objectID == USER_COIN) {
+                        auto eff = typeinfo_cast<EffectGameObject*>(gameObj);
+                        if (eff && eff->getChildByID("rl-suggested")) {
+                              CCSprite::setColor(col);
+                              return;
+                        }
+
+                        CCSprite::setColor({0, 127, 232});
+                        return;
+                  }
+            }
+
+            CCSprite::setColor(col);
       }
 };
